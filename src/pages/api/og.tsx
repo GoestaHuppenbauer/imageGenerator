0a463045f5480dd78a4bdbb1410571f95ab47d03
GoestaHuppenbauer <40520263+GoestaHuppenbauer@users.noreply.github.com>
2023-03-01 19:07:07 +0100

    first Push

---
+++ src/pages/api/og.tsx
@@ -0,0 +1,163 @@
+import { ImageResponse } from '@vercel/og';
+import Link from 'next/link';
+import { NextRequest } from 'next/server';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+// ...Vercel Edge functions config object
+
+// Make sure the font exists in the specified path:
+const font = fetch(new URL('../../assets/Anton-Regular.ttf', import.meta.url)).then(
+  (res) => res.arrayBuffer(),
+);
+  
+  
+  
+  // ...exported function
+  export default async function (req: NextRequest) {
+    const fontData = await font;
+    
+    try {
+      
+    
+          
+      // 1: get the searchParams from the request URL
+      const { searchParams } = new URL(req.url)
+  
+      // 2: Check if title or description are in the params
+      const hasTitle = searchParams.has('title')
+      const hasNumber = searchParams.has('number')
+      const hasUrl = searchParams.has('url')
+      
+  
+      // 3: If so, take the passed value. If not, assign a default
+      const title = hasTitle
+        ? searchParams.get('title')?.slice(0, 100)
+        : 'Some title'
+
+        
+        
+        const image = hasUrl
+        ? searchParams.get('url')
+        :  'http://via.placeholder.com/1200x1200'; 
+           
+
+
+        
+     
+        
+      const number = hasNumber
+        ? searchParams.get('number')?.slice(0, 100)
+        : '00'
+
+        
+        
+  return new ImageResponse(
+    
+    (
+    <div
+        style={{
+          backgroundColor:'#1F1F1F',
+          width: '100%',
+          height: '100%',
+          display:'flex',
+          textAlign: 'center',
+          //alignItems: 'flex-start',
+          //justifyContent: 'flex-start',
+          position: 'relative',
+          objectFit: 'cover',
+        }}
+    >
+        
+        <img
+        id='image'
+          alt="d"
+          height={1200}
+          src={`${image}`}
+           style={{
+            filter: 'grayscale(100%) brightness(50%)',
+            position: 'absolute',
+            display:'flex',
+            objectFit: 'cover',
+            
+          }}
+        />
+
+        <div
+            style={{
+                top:'0',
+                left:'50px',
+                display: 'flex',
+                flexDirection: 'column',
+               top:'100px',
+               
+            }}
+            >
+            <h1
+                    style={{
+                    fontSize: 152,
+                    fontFamily: 'Anton',
+                    color: '#E3C368',
+                    marginBottom: 0,
+                    fontWeight: 900,
+                    }}
+                >
+                    {title}
+            </h1>
+            
+        </div>
+
+        <div
+          style={{
+            marginTop:'auto',
+            marginBottom:'auto',
+            position: 'absolute',
+            display:'flex',
+            verticalAlign:'middle',
+            justifyContent: 'center',
+           bottom:'80px',
+           right:'-240px',
+            transform: 'rotate(-45deg)',
+			backgroundColor: '#E3C368',
+            width:'800px',
+            height:'120px',
+          }}
+        >
+        <p
+            style={{
+            
+                
+              fontFamily: 'Anton',
+              fontSize: 60,
+              color: '#1F1F1F',
+              fontWeight: 700,
+              
+            }}
+          >
+           {number}
+        </p>
+        </div>
+    </div>
+  
+      
+    ),
+    {
+      width: 1200,
+      height: 1200,
+      fonts: [
+        {
+          name: 'Anton',
+          data: fontData,
+          style: 'normal',
+        },
+      ],
+    }
+    ) 
+   
+  } catch (e) {
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    })
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+
+
+
+export default function Home() {
+
+
+  
+
+
+
+
+
+  const [url,setUrl]  = useState('http://via.placeholder.com/1200x1200');
+  const [title,setTitle]  = useState('Filmclub Podcast');
+  const [number,setNumber]  = useState('00')
+
+  const fetchImage = async () => {
+    const FetchUrl = 'https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=' + encodeURIComponent(title)
+    
+    const response = await fetch(FetchUrl);
+    
+
+    const d = await response.json();
+    if(d.total_results!=0){
+      //const id= Math.floor(Math.random() * d.total_results) + 0
+      //console.log(id + ' / ' + d.total_results)
+      const link = d.results[0].backdrop_path;
+    return 'https://image.tmdb.org/t/p/original'+link
+    }
+    else{
+      return 'http://via.placeholder.com/1200x1200'
+    }
+    
+}
+
+  const openInNewTab = (l) => {
+    const newWindow = window.open('/api/og?title='+encodeURIComponent(title)+'&number='+encodeURIComponent(number)+'&url='+encodeURIComponent(l), '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
+  const inputStyle = {
+    height: '1.25em',
+    position:'relative',
+    fontSize: '1.8em',
+    textAlign:'center',
+    border: '0',
+    outline: '0',
+    color: '#fff',
+    background: 'transparent',
+    border:'0.033em #fff solid',
+   left:
+      '50%',
+      transform: 'translate(-50%,0)',
+    marginTop: '1em',
+    marginBottom: '1em',
+    fontFamily: '"Bebas Neue", Arial',
+    verticalAlign: 'middle',
+  }
+  const divStyle = {
+    height: '6.25em',
+    width: '100%',
+    display: 'flex',}
+  const handleGenerate =  async (event) => {
+    event.preventDefault(); 
+    setUrl('http://via.placeholder.com/1200x1200')
+    if(url=='http://via.placeholder.com/1200x1200'){
+      fetchImage().then((d) => { 
+        console.log(url)
+         setUrl(d);openInNewTab(d); })
+  
+    }
+    else{
+      openInNewTab(url)
+    }
+    
+   
+}
+  return (
+    <>
+    <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',height:'100vh'}}>
+    
+      <form style={{
+        height: '18.75em',
+        width: '30em',
+        background: '#fff',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%,-50%)',
+        textTransform: 'uppercase',
+        fontFamily: '"Bebas Neue", Arial',
+        color: '#fff',
+      }}>
+      <h3 style={{color:'darkgray',textAlign:'center',verticalAlign:'middle',marginTop:'2em',marginBottom:'2em'}} >Cover Generator</h3>
+     <div style={divStyle} style={{backgroundColor:'#4daf7c',}}> <input style={inputStyle} placeholder="Filmclub Podcast" value={title} type="text" onChange={({target}) => setTitle(target.value)}></input></div>
+     <div style={divStyle} style={{backgroundColor:'#404241',}}> <input style={inputStyle} placeholder="0" value={number} type="number" onChange={({target}) => setNumber(target.value)}></input></div>
+     <div style={divStyle} style={{backgroundColor:'#e9c85d',}}> <input style={inputStyle}placeholder="Bild url (optional)" value={url} type="url" onChange={({target}) => setUrl(target.value)}></input></div>
+     <div style={divStyle} style={{backgroundColor:'#A30000',}}><span
+  style={{
+    display:'inline-block',
+    marginLeft: '3.3em',
+    marginTop: '1em',
+    marginBottom: '1em',
+    
+    height: '1.25em',
+    width: '9em',
+    fontSize: '2em',
+    textAlign:'center',
+    border: '0',
+    outline: '0',
+    verticalAlign: 'middle',
+    cursor: 'pointer',
+  }} onClick={handleGenerate }>Generieren</span>
+</div>
+      
+      </form>
+   </div>
+    </>
+  )
+}
